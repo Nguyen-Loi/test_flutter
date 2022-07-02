7d9b4df27190388f9749b8f46150692f686edd4a
Hong Loi <86301553+Nguyen-Loi@users.noreply.github.com>
2022-07-02 21:17:33 +0700

    Bai 2. Xuc xac

--- lib/thuc_hanh_screen.dart
+++ lib/thuc_hanh_screen.dart
@@ -1,5 +1,4 @@
-import 'dart:html';
-import 'dart:ui';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -11,93 +10,70 @@ class ThucHanhScreen extends StatefulWidget {
 }
 
 class _ThucHanhScreenState extends State<ThucHanhScreen> {
+  //Phai bo bien tren nay moi chay nha :v
+
+  Random random = Random();
+
   @override
   Widget build(BuildContext context) {
+    //Random mac dinh la 0-100
+    //So bat dau la 0 ma minh can bat dau la 1
+    //Gioi han la 6
+    //Sua lai tu 1 den 6
+    int random1 = random.nextInt(6) + 1;
+    int random2 = random.nextInt(6) + 1;
     //Cac bai tap se code o day
     ///Dau tien la một danh sách hàng cột
     // Thuoc tinh thi nen ghi ngoài children với child
     //Muon long no thi nhu the nay
     //Ctr+ .
     //lam giua the cho nhanh
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    //? Cho no Container day lam gi
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Ghi duong dan anh vo do voi ten cua buc anh
+            //Anh chua co kich thuoc nen khong xuat hien
+            //Chắc ảnh phải ở trong cái gì đó mới hiện lên
+            //Gio chi cho xoa container nhanh ne
+            //Ctr+. => Remove this widget
 
-        ///ghi o day
-        //ko biet no co gì nhấn ctrl+space
-        //ko biet no dung nhu the nao thi ro vao
-        children: [
-          const CircleAvatar(
-            backgroundColor: Color.fromARGB(255, 10, 135, 238),
-            radius: 40,
-          ),
-          
-          Container(
-            color: Colors.white,
-            height: 10,
-          ),
-          Container(
-            color: Colors.red,
-            width: 150,
-            child: const Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                  color: Color.fromARGB(255, 54, 102, 60), fontSize: 16),
-                  textAlign:TextAlign.center ,
+            //Gio lam 1 lan click thi tang hinh anh xuc xac tang len 1
+            InkWell(
+              onTap: () {
+                //Dung setState trong nut nhan moi cap nhat lai man hinh
+                setState(() {
+                  random;
+                });
+              },
+              child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                  //Chi can doi ten anh o day
+                  child: Image.asset('assets/images/dice$random1.png')),
             ),
-          ),
-          Container(
-            color: Colors.white,
-            height: 10,
-          ),
-          Container(
-              color: const Color.fromARGB(255, 218, 206, 206),
-              width: 300,
-              height: 25,
-              child: Row(
-                children: const [
-                  Icon(
-                    Icons.phone,
-                    color: Color.fromARGB(255, 54, 102, 60),
-                    size: 25,
-                  ),
-                  Text(
-                    textAlign: TextAlign.left,
 
-                    '0706027988',
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 54, 102, 60), fontSize: 16),
-                  ),
-                ],
-              )),
-          Container(
-            color: Colors.white,
-            height: 5,
-          ),
-          Container(
-              color: const Color.fromARGB(255, 218, 206, 206),
-              width: 300,
-              height: 25,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.email,
-                    color: Color.fromARGB(255, 54, 102, 60),
-                    size: 25,
-                  ),
-                    Container(
-            color: Color.fromARGB(255, 218, 206, 206), width: 20,
-          ),
-                  Text(
-                    textAlign: TextAlign.left,
-                    'nguyenhongyen333@gmail.com',
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 54, 102, 60), fontSize: 16),
-                  ),
-                ],
-              )),
-        ],
+            Container(
+              width: 150,
+            ),
+            InkWell(
+              onTap: () {
+                setState(() {
+                  random;
+                });
+              },
+              child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                  child: Image.asset('assets/images/dice$random2.png')),
+            ),
+          ],
+        ),
       ),
     );
   }
